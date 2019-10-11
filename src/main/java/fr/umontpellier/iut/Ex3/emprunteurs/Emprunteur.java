101b61c32e647324bfb755c10890a950af16c602
David Binaud <david.binaud@gmail.com>
2019-10-11 16:02:10 +0200

    Ex3 Q3 ajout de la methode getBonus

--- src/main/java/fr/umontpellier/iut/Ex3/emprunteurs/Emprunteur.java
+++ src/main/java/fr/umontpellier/iut/Ex3/emprunteurs/Emprunteur.java
@@ -1,6 +1,8 @@
 package fr.umontpellier.iut.Ex3.emprunteurs;
 
+import fr.umontpellier.iut.Ex3.politiques.AvantageEmprunteur;
+
 public interface Emprunteur {
 
-
+    int getBonus(AvantageEmprunteur av);
 }
--- src/main/java/fr/umontpellier/iut/Ex3/emprunteurs/EmprunteurStandard.java
+++ src/main/java/fr/umontpellier/iut/Ex3/emprunteurs/EmprunteurStandard.java
@@ -1,4 +1,11 @@
 package fr.umontpellier.iut.Ex3.emprunteurs;
 
+import fr.umontpellier.iut.Ex3.politiques.AvantageEmprunteur;
+
 public class EmprunteurStandard implements Emprunteur {
+
+    @Override
+    public int getBonus(AvantageEmprunteur av) {
+        return av.getBonusEmprunteur(this);
+    }
 }
