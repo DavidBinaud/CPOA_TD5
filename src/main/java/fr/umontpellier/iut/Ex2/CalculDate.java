8fcb1413536a5e36128a412756078200d5017707
David Binaud <david.binaud@gmail.com>
2019-10-11 14:34:46 +0200

    Debut EX2 Q3 Creation des classes EmpruntDateFixe et EmpruntSelonTypeDoc

--- src/main/java/fr/umontpellier/iut/Ex2/CalculDate.java
+++ src/main/java/fr/umontpellier/iut/Ex2/CalculDate.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 public class CalculDate {
 
-    static LocalDate ajouter(int nbJours){
+    public static LocalDate ajouter(int nbJours){
         return LocalDate.now().plusDays(nbJours);
     }
 }
